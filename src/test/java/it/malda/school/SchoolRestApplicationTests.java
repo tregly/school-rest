949d8091f095f14ab9d619d67f18cb33831071ba
Giuseppe Maldarelli <g.maldarelli@reply.it>
2022-11-18 11:15:20 +0100

    Initial commit

---
+++ src/test/java/it/malda/school/SchoolRestApplicationTests.java
@@ -0,0 +1,13 @@
+package it.malda.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
