a6b1d948daee6bd780504f5bdadd0b426ffd653c
Adriano Addante <giuseppe.maldarelli.external@enel.com>
2023-01-10 15:02:12 +0100

    All Test Passed Version and 100% SonarQube!

--- src/main/java/it/malda/school/service/StudentService.java
+++ src/main/java/it/malda/school/service/StudentService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -59,10 +60,14 @@ public class StudentService {
             getOne(id);
         }
         student.setId(id);
-        Set<Course> courses = this.studentRepository.findById(id).get().getCoursesRegistration();
-        student.setCoursesRegistration(courses);
+        Optional<Student> studentEntity = this.studentRepository.findById(id);
+        if (studentEntity.isPresent()) {
+            Set<Course> courses = studentEntity.get().getCoursesRegistration();
+            student.setCoursesRegistration(courses);
+        }
         //l'update è possibile farlo su tutto tranne sui corsi iscritti, per quello bisogna fare una chiamata su Course
         return this.studentRepository.save(student);
+
     }
 
     @Transactional
